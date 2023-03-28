ab5e04419d52da67d05dd7e6eb93e22331017a41
akif <akif@5CD20262TT.localdomain>
2023-03-28 15:21:34 +0600

    Added all the timings for all the regions

--- ramadan_dates.js
+++ ramadan_dates.js
@@ -1,16 +1,21 @@
-// values.js
+// // values.js
 
-const valuesList = [
-    "Value 1",
-    "Value 2",
-    "Value 3",
-    "Value 4",
-    "Value 5",
-    // and so on...
-  ];
+// const valuesList = [
+//     "Value 1",
+//     "Value 2",
+//     "Value 3",
+//     "Value 4",
+//     "Value 5",
+//     // and so on...
+//   ];
   
-let currentIndex = 0;
+// let currentIndex = 0;
 
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % valuesList.length;
-}, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+// setInterval(() => {
+//   currentIndex = (currentIndex + 1) % valuesList.length;
+// }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+
+
+
+let bangladeshTime = new Date().toLocaleTimeString("en-US", {timeZone:'Asia/Dhaka',timestyle:'full',hourCycle:'h24'});
+console.log(bangladeshTime);
